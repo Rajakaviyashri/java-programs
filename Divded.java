363a59085cc92a99c933c710fcb859219d3232ac
Raja kaviya shri <yashrisaravanan@gmail.com>
2025-08-25 22:33:39 +0530

    Update Divded.java

--- Divded.java
+++ Divded.java
@@ -1,6 +1,6 @@
 package index1;
 
-public class Divded {
+public class Divded 4{
 public static void main(String[] args) {
      System.out.println("enter number between 100 to 50");
      for(int i=100;i>=50;i--) {
@@ -11,3 +11,4 @@ public static void main(String[] args) {
      }
      }
 }
+
