1c29684d99ddf60ca49fcd18842ba3389fb597ce
Raja kaviya shri <yashrisaravanan@gmail.com>
2025-08-30 00:50:50 +0530

    Add files via upload

---
+++ Arealenghth.java
@@ -0,0 +1,11 @@
+package selfwork;
+
+public class Arealenghth {
+public static void main(String[] args) {
+	int width=4;
+	int length=6;
+	int area=width*length;
+	System.out.println(area);
+	
+}
+}
---
+++ Realexampleofvariables.java
@@ -0,0 +1,17 @@
+package selfwork;
+
+public class Realexampleofvariables {
+public static void main(String[] args) {
+	// entering student details
+	int studentid=13;
+	int age=23;
+	float studentfee=18.9457f;
+	char studentgrade='A';
+	
+	//print the values
+	System.out.println("the student id is:"+ studentid);
+	System.out.println("student age is:" +age);
+	System.out.println("student fee is:"+studentfee);
+	System.out.println("student grade is:" +studentgrade);
+}
+}
